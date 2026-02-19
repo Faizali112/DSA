c4e4abd14e4b7da7535b3f74a85a4ea00f902116
Faizz <156503619+Faizali112@users.noreply.github.com>
2026-02-19 20:11:01 +0530

    Time: 0 ms (100%), Space: 20.4 MB (32.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -5,6 +5,6 @@ class Solution:
 
         for i in range(n-1):
             if prices[i+1]>prices[i]:
-                profit+= prices[i+1]-prices[i]
+                profit+= (prices[i+1]-prices[i])
             
         return profit
